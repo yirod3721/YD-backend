d49fe31e426498fc306c412085f533ffdda60f7f
yirod3721 <ya.31.01.2008@gmail.com>
2025-10-27 16:52:17 +0300

    verification now reports available qualities

--- verif.py
+++ verif.py
@@ -14,9 +14,26 @@ def is_valid_youtube(url):
 
 
 def data_fetch(url):
-    """Fetch video metadata without downloading."""
+    #Fetch video metadata without downloading.
+    verif_opts = {
+        'quiet': True,
+        'skip_download': True
+
+    }
     with yt_dlp.YoutubeDL({'cookiefile': 'cookies.txt'}) as ydl:
         info = ydl.sanitize_info(ydl.extract_info(url, download=False))
+    video_resolution = sorted({
+        f"{f.get('height')}p"
+        for f in info.get("formats", [])
+        if f.get("vcodec") != "none" and f.get("height")
+    }, key=lambda x: int(x.replace("p", "")))
+
+    audio_resolution = sorted({
+        f"{f.get('abr')}kbps"
+        for f in info.get("formats", [])
+        if f.get("acodec") != "none" and f.get("abr")
+    }, key=lambda x: float(x.replace("kbps", "")))
+
 
     # Extract useful fields
     data = {
@@ -24,6 +41,8 @@ def data_fetch(url):
         'uploader': info.get('uploader'),
         'duration': info.get('duration'),           # seconds
         'thumbnail': info.get('thumbnail'),         # URL to image
+        'video_resolution': video_resolution,
+        'audio_resolution': audio_resolution,
     }
     cleanup_temp()
     return data
